864640d593a1ef550aaca838443e080ff415f429
LukeTrynchuk <luke123t@gmail.com>
2020-01-28 18:43:51 -0800

    Add services from blue eyes

---
+++ Runtime/GameStartupScreenService/GameStartupScreenService.cs
@@ -0,0 +1,77 @@
+﻿using DogHouse.Core.Services;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BlueEyes.Services
+{
+    /// <summary>
+    /// GameStartupScreenService is a service that displays 
+    /// a start up screen when the game first begins. Other 
+    /// systems and services may tell the service that the 
+    /// game is ready and a start button will appear.
+    /// </summary>
+    public class GameStartupScreenService : BaseService<IGameStartupScreenService>, IGameStartupScreenService
+    {
+        #region Private Variables
+        [ChildGameObjectsOnly]
+        [SerializeField]
+        [BoxGroup("UI")]
+        [Required("A root for the canvas is required.")]
+        private GameObject m_canvasRoot;
+
+        [ChildGameObjectsOnly]
+        [SerializeField]
+        [BoxGroup("UI")]
+        [Required("A root for the start button is required is required.")]
+        private Button m_startButtonRoot;
+
+        private const float START_GAME_DELAY = 1f;
+        #endregion
+
+        #region Main Methods
+        private void Start()
+        {
+            m_canvasRoot?.gameObject?.SetActive(false);
+            m_startButtonRoot?.gameObject?.SetActive(false);
+            SubscribeToButtons();
+        }
+
+        public override void OnDisable()
+        {
+            base.OnDisable();
+            m_startButtonRoot?.onClick?.RemoveAllListeners();
+            CancelInvoke();
+        }
+
+        public void DisplayUI()
+        {
+            m_canvasRoot?.gameObject?.SetActive(true);
+            m_startButtonRoot?.gameObject?.SetActive(false);
+        }
+
+        public void SetGameReady()
+        {
+            m_startButtonRoot?.gameObject?.SetActive(true);
+        }
+
+        private void StartButtonClicked()
+        {
+            m_startButtonRoot?.onClick?.RemoveAllListeners();
+            Invoke(nameof(BeginGame), START_GAME_DELAY);
+        }
+        #endregion
+
+        #region Utility Methods
+        private void SubscribeToButtons()
+        {
+            m_startButtonRoot?.onClick.AddListener(StartButtonClicked);
+        }
+
+        private void BeginGame()
+        {
+            this.gameObject.SetActive(false);
+        }
+        #endregion
+    }
+}
---
+++ Runtime/GameStartupScreenService/IGameStartupScreenService.cs
@@ -0,0 +1,16 @@
+﻿using DogHouse.Core.Services;
+
+namespace BlueEyes.Services
+{
+    /// <summary>
+    /// IGameStartupScreenService is a service that controls
+    /// when the game startup screen that is displayed to the
+    /// user while the map is being generated and the game
+    /// is being setup.
+    /// </summary>
+    public interface IGameStartupScreenService : IService
+    {
+        void DisplayUI();
+        void SetGameReady();   
+    }
+}
